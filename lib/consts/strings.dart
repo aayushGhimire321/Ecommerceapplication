791f1da469402b3034ad9d742512a75c4fd1195f
Aayush Ghimire <aghimire491@gmail.com>
2024-02-25 21:50:07 +0545

    works going on

---
+++ lib/consts/strings.dart
@@ -0,0 +1,76 @@
+import 'firebase_consts.dart';
+
+const appname = "eMart";
+const appversion = "Version 1.0.0";
+const credits = "@Sujan Gautam";
+const email = "Email";
+const emailHint = "admin@admin.com";
+const password = "Password";
+const passwordHint = "*******";
+const retypePassword = "Retype Password";
+const name = "Name";
+const nameHint = "Your name";
+const forgetPass = "Forget Password";
+const login = "Log in";
+const loggedin = "Logged in successfully";
+const loggedout = "Logged out successfully";
+const logout = "Log out";
+const signup = "Sign up";
+const createNewAccount = "or, create a new account";
+const loginWith = "Log in with";
+const privacyPolicy = "Privacy Policy";
+const termAndCond = "Terms and Conditions";
+const alreadyHaveAccount = "Already have an account?";
+const home = "Home",
+    categories = "Catgories",
+    cart = "Cart",
+    account = "Account";
+
+//home screen strings
+
+const searchanything = "Search anything",
+    todayDeal = "Today's Deal",
+    flashSale = "flash sale",
+    topSellers = "Top Sellers",
+    brand = "Brand",
+    topCategories = "Top Categories",
+    womenDress = "Women Dress",
+    girlsWatches = "Girls Watches",
+    mobilePhone = "Mobile Phone",
+    boysGlasses = "Boys Glasses",
+    tshirts = "Tshirts",
+    girlsDress = "Girls Dresses",
+    featuredProduct = "Featured Product",
+    featuredCategories = "Featured Categories";
+
+// Category Strings
+const womenClothing = "Women Clothing",
+    menClothingFashion = "Men Clothing & Fashion",
+    compAccess = "Computer & Accessories",
+    automobile = "Automobile",
+    kidtoys = "Kids & Toys",
+    sports = "Sports",
+    jewelery = "Jewelery",
+    cellphone = "Cellphone & Tab",
+    furniture = "Furniture";
+
+// item details strings
+const video = "Video",
+    reviews = "Reviews",
+    sellerpolicy = "Seller Policy",
+    returnpolicy = "Return Policy",
+    supportPolicy = "Support Policy",
+    productsyoumaylike = "Products you may also like";
+
+//profile strings
+const wishlist = "My Wishlist",
+    orders = "My Orders",
+    messages = "Messages",
+    oldpass = "Old password",
+    newpass = "New Password";
+
+//payment methods strings
+
+const paypal = "Paypal", stripe = "Stripe", cod = "Cash on delivery";
+
+
